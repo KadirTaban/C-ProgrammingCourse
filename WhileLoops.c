eb352a728a6f4201defcb3c39362e50d83da9dc0
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-10-03 23:04:47 +0300

    whileLoops

---
+++ WhileLoops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int index = 1;
+    while(index <= 5) {
+        printf("%d\n",index);
+        index = index +1;
+    }while(index <= 5);
+    
+
+    return 0;
+
+}
