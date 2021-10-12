7e733f51787b42296562ab19adcbbd0f1e489c75
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-10-12 12:34:31 +0300

    math operators

---
+++ mathOperators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10, b = 100;
+
+    printf("++x = %d \n", ++x);
+    printf("x++ = %d \n", x++);
+
+    printf("--b = %d \n", --b);
+    printf("b-- = %d \n", b--);
+
+    return 0;
+}
