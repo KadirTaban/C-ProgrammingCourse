ed0a45e51249e49972a5552b46ae32a7df3d04a3
Kadir Taban <kadirtaban08@gmail.com>
2021-12-15 14:21:24 +0300

    deneme

--- homeWork_4/twentyFifth.c
+++ homeWork_4/twentyFifth.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main(){
-<<<<<<< HEAD
     int n;
     printf("please given n ;")
-    
+
     scanf("%d",n);
-=======
-    int rows;
 
-    scanf("%d",rows);
->>>>>>> d4ecec2989e988f677362a80439a9366cc8c8670
+    for(int row = 0 ; n<row; row++)
+        {
 
-    for(){
+            
+            int a ;
 
-    }
+            for(int i = 1; ) 
+
+        }
+
+    
     
 }
