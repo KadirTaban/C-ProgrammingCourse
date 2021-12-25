cd5aca6ec31aced0d7425d446a0e054ee3fbb653
Kadir Taban <kadirtaban08@gmail.com>
2021-12-25 11:28:53 +0300

    sixteenth.c is working

--- homeWork_4/sixteenth.c
+++ homeWork_4/sixteenth.c
@@ -4,35 +4,39 @@
 
 int main()
 {
-    float number , x, fac=1,a=1,sum=0,t;
+    float number, x, fac=1,a=1,sum=0,t;
     scanf("%f",&number);
     float terms;
     scanf("%f",&terms);
 
     for(float i = 2; i <= terms ; i=i+2){
+        a = a*-1;
         x = number;
         for(float j = 1; j <i ; j++){
             x = x * number;
 
         }
-
         printf("x üzeri i %f",x);
         fac = 1;
-        fac = fac * a; 
         for(float j = 1; j<i+1 ; j++){
             
             fac = fac * j;
 
         }
         printf("i kadar fac %f",fac);
-        t = x/fac;
-        sum = sum + 1-t;
-        t = t*-1;
+        t = a * (x/fac);
+
+
+        printf("t %f\n",t);
+        sum = sum + t;
+        
 
 
     }
+
+
     
-        printf("sonuc %f",sum);
+        printf("sonuc %f",sum+1);
     
 
 
