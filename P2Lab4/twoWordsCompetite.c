067cf65f820e271574a0a29122f38e68345239ef
Kadir Taban <kadirtaban08@gmail.com>
2022-04-09 11:14:14 +0300

    midterms property

---
+++ P2Lab4/twoWordsCompetite.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[] = "kadir ve sevda";
+
+    int len = strlen(str);
+
+    for(int i = 0 ; i<len; i++){
+        str[i]
+    }
+
+}
