4748c6f64ade8f02be37aa5698136254ea3aa76f
Kadir Taban <kadirtaban08@gmail.com>
2022-04-18 16:10:29 +0300

    lab2

--- Sem2/lab2/largestElement.c
+++ Sem2/lab2/largestElement.c
@@ -1,38 +1,35 @@
 
 //Q2: Write a recursion program that finds the largest element in the entered array in C language.
-#include <stdio.h>
-#define N 5 
+#include<stdio.h>
+#define MAX 100
 
-int largestNumber(int arr[], int size);
-
-
+int getMaxElement(int []);  // takes array of int as parameter
+int size;
 int main(){
-    
-    int numbers[N];
-    int size = 5;
+    int arr[MAX], max, i;
+    printf("\n\nEnter the size of the array: ");
+    scanf("%d", &size);
+    printf("\n\nEnter %d elements\n\n", size);
 
-    for(int i = 0; i<5;i++){
-        scanf("%d",&numbers[i]); 
+    for(i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
     }
-    printf("%d",largestNumber(numbers, 5));
-
+    
+    max = getMaxElement(arr);   // passing the complete array as parameter
+    printf("\n\nLargest element of the array is %d\n\n", max);
+    return 0;
 }
 
-int largestNumber(int arr[], int size ){
-    int max;
-    
-    if(0 < size){
-        if (max < arr[size]){
+int getMaxElement(int a[]){
+    static int i = 0, max =- 9999;  // static int max=a[0] is invalid
+    if(i < size)   // till the last element
+    {
+        if(max < a[i])
+        max = a[i];
 
-        max= arr[size];
-        
-        
-        }
-        return largestNumber(arr,size-1);
+        i++;    // to check the next element in the next iteration
+        getMaxElement(a);   // recursive call
     }
-    else{
-        return max;
-    }
-
-    
+    return max;
 }
