9509bd48ae4b67725bcdcb95298b926f9c1fa3ef
Kadir Taban <kadirtaban08@gmail.com>
2022-05-09 20:57:09 +0300

    commit

---
+++ Sem2/structs/student.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct student {
+
+    int no;
+    char name[30];
+    int midtermGrade;
+    int finalGrade;
+    int average;
+
+};
+
+typedef struct student stu;
+
+void(stu*)
