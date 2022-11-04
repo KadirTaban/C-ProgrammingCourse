240f459c6ae8f370b073e1782365760e4c64f4da
KadirTaban <kadirtaban08@gmail.com>
2022-11-04 19:42:33 +0300

    queue examples

---
+++ DataStructures/Queue/Queue.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define QUEUE_SIZE 10
+
+
+struct node{
+    int data;
+    struct node *next;
+};
+typedef struct node node;
+
+
+typedef struct{
+    int cnt;
+    struct node *front;
+    struct node *rear;
+}que;
+
+
+void initialize(que *q){
+        q->cnt = 0;
+        q->front = NULL;
+        q->rear = NULL;
+    }
+
+int isFull(que *q){
+    if(q->cnt == QUEUE_SIZE){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int isEmpty(que *q){
+    if(q->cnt == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void enqueue(que *q, int x){
+    if (isFull(q)){
+        printf("Queue is full");
+    }else{
+        struct node* temp = malloc(sizeof(node));
+        temp->data = x;
+        temp->next = NULL;
+        if(isEmpty(q)){
+            q->front = q->rear = temp;
+        }else{
+            q->rear->next= temp;
+            q->rear = temp;
+        }
+        q->cnt++;
+    }
+
+}
+
+void dequeue(que *q){
+    if (isEmpty(q)){
+        printf("Queue is empty");
+    }else{
+        node *temp = q->front;
+        int x = temp->data;
+        q->front = temp->next;
+        free(temp);
+        q->cnt--;
+
+    }
+}
+void display(node *head)
+{
+    if(head == NULL)
+    {
+        printf("NULL\n");
+    }
+    else
+    {
+        printf("%d\n", head -> data);
+        display(head->next);
+    }
+}
+
+
+int main(){
+
+    que *q;
+
+    q = malloc(sizeof(que));
+    initialize(q);
+    enqueue(q,10);
+    enqueue(q,20);
+    display(q->front);
+    dequeue(q);
+    display(q->front);
+    return 0;
+    
+}
---
+++ DataStructures/Queue/QueueArray.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define QUEUE_SIZE 10
+
+struct que
+{
+    int cnt;
+    int front,rear;
+    int data[QUEUE_SIZE];
+};
+
+typedef struct que que;
+
+void initialize(que *q){
+    q->cnt = 0 ;
+    q->rear = -1;
+    q->front = 0;
+}
+int isFull(que *q){
+    if(q->cnt == QUEUE_SIZE){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int isEmpty(que *q){
+    if(q->cnt == 0 ){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+void enqueue(que *q, int x){
+    if(isFull(q)){
+        printf("Queue is full. ");
+    }else{
+        q->rear++;
+        q->cnt++;
+        if(q->rear == QUEUE_SIZE){
+            q->rear = 0;
+        }
+        q->data[q->rear]=x;
+    }
+}
+
+
+int dequeue(que *q){
+    if(isEmpty(q)){
+        printf("Queue is empty");
+        return 0;
+    }else{
+        int x = q->data[q->front];
+        q->front++;
+        q->cnt--;
+
+        if(q->front ==  QUEUE_SIZE){
+            q->front = 0;
+        }
+        return x;
+    }
+}
+
+void display(que *q)
+{
+    int i;
+    if(isEmpty(q)){
+        printf("\n que is empty\n");
+    }
+    else{
+        for (int i = q->front; i<=q->rear; i++){
+            printf("%d", q->data[i]);
+        }
+    }
+} 
+
+
+int main(){
+    int num;
+    que *q = (que*)malloc(sizeof(que));
+    initialize(q);
+    scanf("%d", &num);
+    enqueue(q, num);
+    scanf("%d", &num);
+    enqueue(q, num);    
+    display(q);
+    num = dequeue(q);
+    printf("\nElement at front: %d\n", num);
+    display(q);
+
+
+
+
+
+
+
+
+}
+
